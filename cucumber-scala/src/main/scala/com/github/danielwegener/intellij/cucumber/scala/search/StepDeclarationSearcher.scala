c8c932d856bbd592750935c662265252231c40c4
Peter Jakubco <pjakubco@gmail.com>
2020-04-26 15:12:15 +0200

    Find usages now works also for infix expressions

--- cucumber-scala/src/main/scala/com/github/danielwegener/intellij/cucumber/scala/search/StepDeclarationSearcher.scala
+++ cucumber-scala/src/main/scala/com/github/danielwegener/intellij/cucumber/scala/search/StepDeclarationSearcher.scala
@@ -3,38 +3,40 @@ package com.github.danielwegener.intellij.cucumber.scala.search
 import com.github.danielwegener.intellij.cucumber.scala.ScCucumberUtil
 import com.github.danielwegener.intellij.cucumber.scala.steps.ScStepDefinition
 import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.pom.{PomDeclarationSearcher, PomTarget}
-import com.intellij.psi.{PsiDocumentManager, PsiElement}
 import com.intellij.psi.util.{CachedValueProvider, CachedValuesManager}
+import com.intellij.psi.{PsiDocumentManager, PsiElement}
 import com.intellij.util.Consumer
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
-
-import scala.util.Try
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScInfixExpr, ScMethodCall}
 
 class StepDeclarationSearcher extends PomDeclarationSearcher {
+  private val LOG = Logger.getInstance(classOf[StepDeclarationSearcher])
 
   override def findDeclarationsAt(psiElement: PsiElement, offsetInElement: Int, consumer: Consumer[PomTarget]): Unit = {
     val host = InjectedLanguageManager.getInstance(psiElement.getProject).getInjectionHost(psiElement)
-
-    // Currently works just for literal regexes
     val hostOrElement = Option(host).getOrElse(psiElement)
 
-    hostOrElement.getParent match {
-
-      case literal: ScLiteral =>
-        for {
-          parent <- Option(literal.getParent) //~literal
-          pparent <- Option(parent.getParent) //(~literal)
-          ppparent <- Option(pparent.getParent) //When(~literal)
-        } yield ppparent match {
-          case method: ScMethodCall if ScCucumberUtil.isStepDefinition(method) =>
-            Try(getCachedStepDefinition(method).foreach(consumer.consume))
-          case _ =>
-        }
-
-      case _ =>
+    val stepDefinition = hostOrElement.getParent match {
+      case literal: ScLiteral => findStepDefinition(literal)
+      case expr: ScInfixExpr => findStepDefinition(expr)
+      case _ => None
     }
+
+    stepDefinition.foreach(consumer.consume)
+  }
+
+  private def findStepDefinition(element: ScExpression): Option[ScStepDefinition] = {
+    for {
+      parent <- Option(element.getParent) //~literal
+      pparent <- Option(parent.getParent) //(~literal)
+      ppparent <- Option(pparent.getParent) //When(~literal)
+
+      if ScCucumberUtil.isStepDefinition(ppparent)
+      stepDefinition <- getCachedStepDefinition(ppparent.asInstanceOf[ScMethodCall])
+
+    } yield stepDefinition
   }
 
   private def getCachedStepDefinition(statement: ScMethodCall): Option[ScStepDefinition] = {
