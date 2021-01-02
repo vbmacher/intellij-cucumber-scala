aa30b3210ac56e4e93833907e2cd16b0d417de02
Peter Jakubco <pjakubco@gmail.com>
2021-01-02 17:17:14 +0100

    Fix custom param type regex

--- example/src/test/scala/CustomTypes.scala
+++ example/src/test/scala/CustomTypes.scala
@@ -1,11 +1,11 @@
 import io.cucumber.scala.ScalaDsl
 
 trait CustomTypes extends ScalaDsl {
-  ParameterType("speed", ".*\\s+km/h") { speed: String => Speed(speed.split(' ').head.toDouble) }
-  ParameterType("time", ".*\\s+h") { time: String => Time(time.split(' ').head.toDouble) }
+  ParameterType("speed", "\\\\d+km/h") { speed: String => Speed(speed.split(' ').head.toDouble) }
+  ParameterType("time", "\\\\d+h") { time: String => Time(time.split(' ').head.toDouble) }
 
-  ParameterType("speed-m-s", ".*\\s+m/s") { speed: String => Speed(speed.split(' ').head.toDouble) }
-  ParameterType("time-s", ".*\\s+s") { time: String => Time(time.split(' ').head.toDouble) }
+  ParameterType("speed-m-s", "\\\\d+m/s") { speed: String => Speed(speed.split(' ').head.toDouble) }
+  ParameterType("time-s", "\\\\d+s") { time: String => Time(time.split(' ').head.toDouble) }
 }
 
 case class Speed(kmPerHour: Double)
